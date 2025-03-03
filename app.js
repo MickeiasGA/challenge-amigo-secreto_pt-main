8d16186b7bbde15c91cc3fcf1a827c554d72b8eb
MickeiasGA <cc22144@g.unicamp.br>
2025-03-03 18:47:45 -0300

    Terminado

--- app.js
+++ app.js
@@ -2,19 +2,38 @@
 let amigos = [];
 
 function adicionarAmigo() {
-    let amigo = document.getElementById('amigo').value;
-    amigos.push(amigo);
-    amigo = '';
+    let amigo = document.getElementById('amigo');
+    amigos.push(amigo.value);
+    amigo.value = "";
 
     let ul = document.getElementById('listaAmigos');
     ul.innerHTML = "";
     for (let i = 0; i < amigos.length; i++) {
         let li = document.createElement('li');
+        li.setAttribute("id", `${i}`)
         li.innerHTML = amigos[i];
         ul.appendChild(li);
     }
 }
 
 function sortearAmigo() {
-    //let amigo = 
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = ""
+
+    let n = Math.floor(Math.random() * amigos.length);
+    document.getElementById('listaAmigos').children[n].innerHTML  = "";
+
+    let li = document.createElement('li');
+    li.innerHTML = amigos[n];
+    resultado.appendChild(li);
+
+    amigos.splice(n, 1);
+    let ul = document.getElementById('listaAmigos');
+    ul.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.setAttribute("id", `${i}`)
+        li.innerHTML = amigos[i];
+        ul.appendChild(li);
+    }
 }
